caccf50a3039336b8fd546ee638f3c79d0af7666
Spraynard <kellan.martin@gmail.com>
2023-01-05 01:40:12 -0500

    Modifying feed.test.js

--- tests/feed.test.js
+++ tests/feed.test.js
@@ -1,4 +1,8 @@
-const AbstractFeed = require('../src/Feeds/AbstractFeed')
+
+import AbstractFeed from "../src/Feeds/AbstractFeed";
+import GoogleFeed from "../src/Feeds/GoogleFeed";
+
+class TestFeed extends GoogleFeed {}
 
 describe("Feed Tests", () => {
     test("Abstract feed will error out if you try and construct it", () => {
