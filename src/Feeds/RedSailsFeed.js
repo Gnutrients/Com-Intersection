e5b5d8fe7c25196d9855cf565a19ca8ef24f242f
Spraynard <kellan.martin@gmail.com>
2023-01-05 01:40:19 -0500

    Modifying RedSailsFeed.js
    Removing headers

--- src/Feeds/RedSailsFeed.js
+++ src/Feeds/RedSailsFeed.js
@@ -6,11 +6,6 @@ class RedSailsFeed extends RssFeed
         super("Red Sails", {
             host: "redsails.org",
             path: "/rss.xml",
-            headers: {
-                "Accept": "text/html,application/xhtml+xml,application/xml",
-                "Accept-Language": "en-US",
-                "Upgrade-Insecure-Requests" : 1
-            }
         })
     }
 }
