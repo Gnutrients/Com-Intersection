91acf3db308b343b5f377442d110b12d8ea699ee
Spraynard <kellan.martin@gmail.com>
2023-01-05 01:40:10 -0500

    Modifying Article.js
    Implementing the object in our file

--- src/Article.js
+++ src/Article.js
@@ -1,13 +1,30 @@
 class Article {
+    /**
+     * This class contains information about a single Article
+     * 
+     * @param {string} title Title of the article
+     * @param {string} body Body of the article
+     * @param {string} link Link to the article
+     * @param {string|Date} date Date the article was written
+     */
     constructor(
-        description,
-        title
+        title,
+        body,
+        link = "",
+        date = null,
+
     )
     {
-        this.description = description
+        this.body  = body
+        this.date  = date
+        this.link  = link
         this.title = title
+
+        if (!Date.prototype.isPrototypeOf(this.date))
+        {
+            this.date = new Date(this.date)
+        }
     }
 }
-
 
 export default Article;
