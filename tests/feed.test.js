c4474c072bdb0c7d324e6df3b20be0bb3026d6ca
Spraynard <kellan.martin@gmail.com>
2023-01-05 01:40:18 -0500

    Modifying feed.test.js
    Adding testing material

--- tests/feed.test.js
+++ tests/feed.test.js
@@ -1,11 +1,28 @@
 
 import AbstractFeed from "../src/Feeds/AbstractFeed";
-import GoogleFeed from "../src/Feeds/GoogleFeed";
+import Publisher from "../src/Publisher";
 
-class TestFeed extends GoogleFeed {}
+class TestFeed extends AbstractFeed {
+    constructor() {
+        super("Test Feed", {
+            host : "www.google.com"
+        })
+    }
+
+    parse_response(data) {
+        return new Publisher([])
+    }
+}
 
 describe("Feed Tests", () => {
     test("Abstract feed will error out if you try and construct it", () => {
         expect(() => { new AbstractFeed() }).toThrow();
     })
+
+    test("Feed will have a tag name", () => {
+        let test_feed = new TestFeed();
+
+        expect(test_feed.get_tag_name()).toBe("test-feed")
+    })
+
 })
