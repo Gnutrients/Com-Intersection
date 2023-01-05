33cf18073b10f14adbd5c77c7c89a9fe155e4f2e
Spraynard <kellan.martin@gmail.com>
2023-01-05 01:40:11 -0500

    Modifying index.js
    Implementing near minimum viable product

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 import express from "express"
+
+// Importing our feeds
 import FightBackNewsFeed from "./src/Feeds/FightBackNewsFeed.js"
+import WorkersWorldFeed from "./src/Feeds/WorkersWorldFeed.js"
+// import FightingWordsNewsFeed from "./src/Feeds/FightingWordsNewsFeed.js"
 
 const app = express()
 const port = 80
-const feeds = [
-    FightBackNewsFeed
-]
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('home');
+    const feeds = [
+        FightBackNewsFeed,
+    ]
+
+    const publishers_available = feeds.map((feed) => feed.name)
+
+    Promise.all(feeds.map((feed) => {
+        return feed.obtain_feed_data()
+    })).then((publishers) => {
+        res.render('home', {
+            // List of feeds that are available to use
+            publishers_available,
+            // List of publishers
+            publishers
+        })
+    })
 });
 
 app.listen(port, () => {
