624ce6b290448e6c34f7230ffc27fefb949a6681
Spraynard <kellan.martin@gmail.com>
2023-01-07 14:56:32 -0500

    Adding CommunistPartyUSAFeed.js

---
+++ src/Feeds/CommunistPartyUSAFeed.js
@@ -0,0 +1,13 @@
+import RssFeed from "./RssFeed.js"
+
+class CommunistPartyUSAFeed extends RssFeed 
+{
+    constructor() {
+        super("Communist Party USA", {
+            host: "www.cpusa.org",
+            path: "/feed/?post_type=article",
+        })
+    }
+}
+
+export default new CommunistPartyUSAFeed()
