76831f4177f9354c1638345db5a591aebe706d45
Spraynard <kellan.martin@gmail.com>
2023-01-07 15:01:21 -0500

    Modifying index.js
    Adding feeds
    
    filtering out null data from feeds when we obtain feed data

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express"
 
 // Importing our feeds
+import CommunistPartyUSAFeed from "./src/Feeds/CommunistPartyUSAFeed.js"
 import CubanMinistryOfJusticeFeed from "./src/Feeds/CubanMinistryOfJusticeFeed.js"
 import FightBackNewsFeed from "./src/Feeds/FightBackNewsFeed.js"
+import LiberationNewsFeed from "./src/Feeds/LiberationNewsFeed.js"
 import RedSailsFeed from "./src/Feeds/RedSailsFeed.js"
 import WorkersWorldFeed from "./src/Feeds/WorkersWorldFeed.js"
+import WorldSocialistWebSiteFeed from "./src/Feeds/WorldSocialistWebSiteFeed.js"
 
 const app = express()
 const port = process.env.PORT || 4001;
@@ -18,19 +21,24 @@ app.get('/', (req, res, next) => {
         FightBackNewsFeed,
         RedSailsFeed,
         WorkersWorldFeed,
-        CubanMinistryOfJusticeFeed
+        CubanMinistryOfJusticeFeed,
+        LiberationNewsFeed,
+        CommunistPartyUSAFeed,
+        WorldSocialistWebSiteFeed
     ]
 
     const publishers_available = feeds.map((feed) => [feed.name, feed.get_tag_name()])
     
     Promise.all(feeds.map((feed) => {
         return feed.obtain_feed_data()
-    })).then((publishers) => {
+    })).then( publishers => publishers.filter( value => {
+        return value;
+    })).then( filtered_publishers => {
         res.render('home', {
             // List of feeds that are available to use
             publishers_available,
             // List of publishers
-            publishers
+            publishers : filtered_publishers
         })
     }).catch(reason => next(reason))
 });
