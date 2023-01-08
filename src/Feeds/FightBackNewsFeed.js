3ad3911241e4a7b6ebd4e94366380c90602110af
Spraynard <kellan.martin@gmail.com>
2023-01-08 12:18:32 -0500

    Modifying FightBackNewsFeed.js
    Extends RssFeed now so removing implementation details

--- src/Feeds/FightBackNewsFeed.js
+++ src/Feeds/FightBackNewsFeed.js
@@ -1,38 +1,13 @@
-import AbstractFeed from "./AbstractFeed.js";
-import Article from "../Article.js";
-import Publisher from "../Publisher.js";
-import { XMLParser } from "fast-xml-parser";
-
+import RssFeed from "./RssFeed.js";
 /**
  * News feed for Fight Back! News
  */
-class FightBackNewsFeed extends AbstractFeed {
+class FightBackNewsFeed extends RssFeed {
     constructor() {
         super("Fight Back News", {
             host : "www.fightbacknews.org",
             path : "/feed.xml"
         });
-    }
-
-    parse_response(data) {
-        const parser = new XMLParser();
-        const parsed = parser.parse(data);
-
-        const articles = parsed.rss.channel.item.map((value, index) => {
-            return new Article(
-                value.title,
-                value.description,
-                value.link,
-                value.pubDate
-            )
-        });
-
-        return new Publisher(articles, {
-            description : parsed.rss.channel.description,
-            language    : parsed.rss.channel.language,
-            link        : parsed.rss.channel.link,
-            title       : this.name,
-        })
     }
 }
 
