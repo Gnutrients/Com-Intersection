6579fdccf26825b7132b35857457883df4aa6e70
Gnutrients <copswatch@protonmail.com>
2023-01-14 18:40:08 -0500

    Modifying WebScraperFeed.js
    Adding in _load_html and _make_request method

--- src/Feeds/Base/WebScraperFeed.js
+++ src/Feeds/Base/WebScraperFeed.js
@@ -1,6 +1,28 @@
 import AbstractFeed from "./AbstractFeed.js";
+import { load, CheerioAPI } from "cheerio";
 
+/**
+ * Base class that provides classes that extend this class with inherited 
+ * methods that allow for easier scraping of data
+ * 
+ * Instead of raw data coming through to the 
+ */
 class WebScraperFeed extends AbstractFeed
 {
-    
+    /**
+     * Loads HTML into Cheerio and returns the Cheerio API
+     * 
+     * @param {string} html_string Any string of HTML
+     * @returns {CheerioAPI}
+     */
+    _load_html(html_string) {
+        return load(html_string);
+    }
+
+    _make_request(options) {
+        return super(options)
+                .then(data => this._load_html(data));
+    }
 }
+
+export default WebScraperFeed
