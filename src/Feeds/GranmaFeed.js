cbaa584d9a48f27e7b240ff586bd14c4ecb44c7e
Gnutrients <copswatch@protonmail.com>
2023-01-19 00:20:27 -0500

    v1.4.0 modifications
    Adding favicon.ico
    
    adding social media links for GranmaFeed.js
    
    Upping version to v1.4.1 in package.json

--- src/Feeds/GranmaFeed.js
+++ src/Feeds/GranmaFeed.js
@@ -1,6 +1,9 @@
 import RssFeed from "./Base/RssFeed.js";
 
 class GranmaFeed extends RssFeed {
+    twitter_link = "https://twitter.com/Granma_English"
+    facebook_link = "https://www.facebook.com/pages/Granma-English/230559647101175"
+    
     constructor() {
         super('Granma', {
             host: "en.granma.cu",
